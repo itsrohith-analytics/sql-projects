998ee255609e40eb27062e058ea520bc2667137a
Rohith J <rohithrh45@gmail.com>
2026-01-13 10:37:28 +0530

    Added College DB schema

--- college-database/college-database/schema.sql
+++ college-database/college-database/schema.sql
@@ -1,1 +1,44 @@
+create database collegedb;
+use collegedb;
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(100),
+    HOD VARCHAR(100)
+);
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    DateOfBirth DATE,
+    Gender VARCHAR(10),
+    Phone VARCHAR(15),
+    Email VARCHAR(100),
+    Address VARCHAR(255)
+);
 
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100),
+    Duration VARCHAR(50),
+    DepartmentID INT,
+    Credits INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+);
+
+CREATE TABLE Faculty (
+    FacultyID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Phone VARCHAR(15),
+    Email VARCHAR(100),
+    Designation VARCHAR(100),
+    DepartmentID INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+);
+CREATE TABLE Enrollment (
+    EnrollmentID INT PRIMARY KEY,
+    StudentID INT,
+    CourseID INT,
+    EnrollmentDate DATE,
+    Grade VARCHAR(2),
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
+);
